63de5945d060b465b0b368ce6aee9dcb683d87f5
JiChan Lee <bbster12@gmail.com>
2021-06-26 00:43:23 +0900

    Model Field 탐구

--- study/models.py
+++ study/models.py
@@ -2,5 +2,8 @@ from django.db import models
 
 
 class MyModel(models.Model):
-    name = models.CharField(max_length=10, blank=True, null=True)
-    age = models.IntegerField(blank=True, null=True)
+    name = models.CharField(max_length=50, null=True)
+    age = models.IntegerField(null=True)
+    boolean_field = models.BooleanField(default=False)
+    date_field = models.DateField(null=True)
+    date_time_field = models.DateTimeField(null=True)
